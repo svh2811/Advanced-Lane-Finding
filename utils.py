c4a0644514c9156631d4ae2afc713ff500a7a124
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-08-31 16:18:03 -0400

    Update: massive refactoring | utils file now actually contain only utilies function

--- utils.py
+++ utils.py
@@ -1,160 +1,62 @@
 import numpy as np
 import cv2
-import glob
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import tempfile
 
+from moviepy.editor import VideoFileClip
+
+
+def extract_video_frames(video_file_path, video_length, ts,
+                            video_frame_save_dir):
+    video = VideoFileClip(video_file_path)  # frame_rate = 25
+
+    for t_s in np.arange(0.0, video_length, ts):
+        # save frame at t = t_s as JPEG
+        video.save_frame('video_frame_save_dir/{:02d}.jpg'\
+                            .format(int(t_s)), t=t_s)
+
+
+def color_in_range(image, low_threshold, high_threshold):
+    mask = cv2.inRange(image, low_threshold, high_threshold)
+    res = cv2.bitwise_and(image, image, mask=mask)
+    return res
+
+
+def get_plot_as_np_array(plt):
+    fileName = tempFile.gettempdir() + "/plot.png"
+    plt.savefig(fileName)
+    return bgr_to_rgb(cv2.imread(fileName))
 
 
 """
-nx : the number of inside corners along x-axis
-ny : the number of inside corners along y-axis
-dir: directory contain all chessboard images
-     which are going to be used for camera_calibration
+images: list of tuples, where each tuple has image title
+        and the image itself
 """
-def get_camera_calibration_matrix(nx, ny,
-    chessboard_input_dir, chessboard_output_dir = None):
-
-    # Arrays to store object points and image point sfrom all the images
-    objpoints = [] # 3D point in real world space
-    imgpoints = [] # 2D point in image plane
-
-    # prepare object points, like
-    # (0, 0, 0), (1, 0, 0), (2, 0, 0)...(ny-1, nx-1, 0)
-    objp = np.zeros((nx * ny, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:ny, 0:nx].T.reshape(-1, 2)
-
-    images = glob.glob(chessboard_input_dir + "/*.jpg")
-    imageSize = None
-
-    for img_fname in images:
-        fname = get_file_name_from_path(img_fname)
-        print("Calibrating Image: ", fname)
-        img = cv2.imread(img_fname)
-        imageSize = img.shape
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#cv2.findChessboardCorners
-        ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-
-        # If found, draw corners
-        if ret == True:
-            objpoints.append(objp)
-            imgpoints.append(corners)
-            # https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#cv2.drawChessboardCorners
-            if chessboard_output_dir is not None:
-                # Draw and display the corners
-                cv2.drawChessboardCorners(img, (nx, ny), corners, ret)
-                cv2.imwrite(chessboard_output_dir + "/" + fname + ".jpg", img)
-
-    # https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html#cv2.calibrateCamera
-    return cv2.calibrateCamera(objpoints, imgpoints,
-                                (imageSize[1], imageSize[0]), None, None)
-
-
-def undistort_images(cameraMatrix, distCoeffs,
-                    images_dir, output_images_dir = None):
-    images = glob.glob(images_dir + "/*.jpg")
-    for img_fname in images:
-        fname = get_file_name_from_path(img_fname)
-        print("Undistorting Image: ", fname)
-        img = cv2.imread(img_fname)
-        # https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#cv2.undistort
-        dst = cv2.undistort(img, cameraMatrix, distCoeffs, None, cameraMatrix)
-        if output_images_dir is not None:
-            cv2.imwrite(output_images_dir + "/" + fname + ".jpg", dst)
-
-
-def gradient_thresh(img, orient="x", sobel_kernel=3, thresh=(20, 100)):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    if orient == 'x':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel))
-    if orient == 'y':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel))
-    abs_sobel = np.absolute(abs_sobel)
-    """
-    It's not entirely necessary to convert to 8-bit (range from 0 to 255)
-    but in practice, it can be useful in the event that we've written a
-    function to apply a particular threshold, and if we want it to work the
-    same on input images of different scales, like jpg vs. png.
-    we could just as well choose a different standard range of values,
-    like 0 to 1 etc.
-    """
-    scaled_sobel = np.uint8(255 * abs_sobel/np.max(abs_sobel))
-    binary_output = np.zeros_like(binary_output)
-    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-    return binary_output
-
-
-def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize = sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize = sobel_kernel)
-    gradmag = np.sqrt(sobelx**2 + sobely**2)
-    scale_factor = np.max(gradmag)/255
-    gradmag = (gradmag/scale_factor).astype(np.uint8)
-    binary_output = np.zeros_like(gradmag)
-    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
-    return binary_output
-
-
-# Define a function to threshold an image for a given range and Sobel kernel
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    binary_output =  np.zeros_like(absgraddir)
-    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
-    return binary_output
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def plot_images(img1_loc, img2_loc, img1_title, img2_title, fileName):
-    plt.rcParams["figure.figsize"] = [19, 8]
+def plot_images_along_row(images, figsize = [19, 8]):
+    plt.rcParams["figure.figsize"] = figsize
     plt.tight_layout()
     fig = plt.figure()
-    a = fig.add_subplot(1,2,1)
-    img = mpimg.imread(img1_loc)
-    imgplot = plt.imshow(img)
-    a.set_title(img1_title)
-    a = fig.add_subplot(1,2,2)
-    img = mpimg.imread(img2_loc)
-    imgplot = plt.imshow(img)
-    a.set_title(img2_title)
+    n = len(images)
+    for i in range(n):
+        a = fig.add_subplot(1, n, i+1)
+        if (len(images[i][1].shape) == 2):
+            imgplot = plt.imshow(images[i][1], cmap='gray',\
+                                            vmin = 0, vmax = 1)
+        else:
+            imgplot = plt.imshow(images[i][1])
+        a.set_title(images[i][0])
     plt.show()
-    fig.savefig(fileName, bbox_inches = 'tight')
     plt.close()
+
+
+def bgr_to_rgb(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+
+
+def blur(img, kernel):
+    return cv2.GaussianBlur(img, (kernel, kernel), 0)
 
 
 def get_file_name_from_path(path):
